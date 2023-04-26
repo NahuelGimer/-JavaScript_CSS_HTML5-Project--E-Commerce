eea845d0b3dc2021b9d351f53dda29dd90e680d6
NahuelGimer <104289028+NahuelGimer@users.noreply.github.com>
2023-04-26 17:53:41 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,22 @@
 /*=============== SWIPER PRODUCTS ===============*/
 
 var swiperCategories = new Swiper(".tab__item", {
-	slidesPerView: 24,
+	centeredSlides: true,
+	spaceBetween: 10,
 	loop: true,
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
 	breakpoints: {
-		0: {
-			slidesPerView: 1,
-			spaceBetween: 20,
-		},
 		320: {
 			slidesPerView: 1,
-			spaceBetween: 10,
+			spaceBetween: 20,
+			centeredSlides: true,
 		},
 		720: {
 			slidesPerView: 2,
 			spaceBetween: 32,
-		},
-		1200: {
-			slidesPerView: 3,
-			spaceBetween: 15,
 		},
 	},
 });
