852dfbb6b590554d50a0ae85c5276c955a94e877
NahuelGimer <104289028+NahuelGimer@users.noreply.github.com>
2023-04-29 03:09:52 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,233 @@
+let loginF = false,
+	cartF = false,
+	heartF = false;
+
+function login() {
+	loginF = !loginF;
+	if (loginF) {
+		document.getElementById("login-ad").classList.remove("up");
+	} else {
+		document.getElementById("login-ad").classList.remove("up");
+	}
+}
+
+function cart1() {
+	cartF = !cartF;
+	if (cartF) {
+		document.getElementById("cart1").classList.remove("fi-rs-shopping-cart");
+		document.getElementById("cart1").classList.add("fi-ss-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("cart1").classList.remove("fi-ss-shopping-cart");
+		document.getElementById("cart1").classList.add("fi-rs-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function heart1() {
+	heartF = !heartF;
+	if (heartF) {
+		document.getElementById("heart1").classList.remove("fi-rs-heart");
+		document.getElementById("heart1").classList.add("fi-ss-heart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("heart1").classList.remove("fi-ss-heart");
+		document.getElementById("heart1").classList.add("fi-rs-heart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function cart2() {
+	cartF = !cartF;
+	if (cartF) {
+		document.getElementById("cart2").classList.remove("fi-rs-shopping-cart");
+		document.getElementById("cart2").classList.add("fi-ss-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("cart2").classList.remove("fi-ss-shopping-cart");
+		document.getElementById("cart2").classList.add("fi-rs-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function heart2() {
+	heartF = !heartF;
+	if (heartF) {
+		document.getElementById("heart2").classList.remove("fi-rs-heart");
+		document.getElementById("heart2").classList.add("fi-ss-heart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("heart2").classList.remove("fi-ss-heart");
+		document.getElementById("heart2").classList.add("fi-rs-heart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function cart3() {
+	cartF = !cartF;
+	if (cartF) {
+		document.getElementById("cart3").classList.remove("fi-rs-shopping-cart");
+		document.getElementById("cart3").classList.add("fi-ss-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("cart3").classList.remove("fi-ss-shopping-cart");
+		document.getElementById("cart3").classList.add("fi-rs-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function heart3() {
+	heartF = !heartF;
+	if (heartF) {
+		document.getElementById("heart3").classList.remove("fi-rs-heart");
+		document.getElementById("heart3").classList.add("fi-ss-heart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("heart3").classList.remove("fi-ss-heart");
+		document.getElementById("heart3").classList.add("fi-rs-heart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function cart4() {
+	cartF = !cartF;
+	if (cartF) {
+		document.getElementById("cart4").classList.remove("fi-rs-shopping-cart");
+		document.getElementById("cart4").classList.add("fi-ss-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("cart4").classList.remove("fi-ss-shopping-cart");
+		document.getElementById("cart4").classList.add("fi-rs-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function heart4() {
+	heartF = !heartF;
+	if (heartF) {
+		document.getElementById("heart4").classList.remove("fi-rs-heart");
+		document.getElementById("heart4").classList.add("fi-ss-heart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("heart4").classList.remove("fi-ss-heart");
+		document.getElementById("heart4").classList.add("fi-rs-heart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function cart5() {
+	cartF = !cartF;
+	if (cartF) {
+		document.getElementById("cart5").classList.remove("fi-rs-shopping-cart");
+		document.getElementById("cart5").classList.add("fi-ss-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("cart5").classList.remove("fi-ss-shopping-cart");
+		document.getElementById("cart5").classList.add("fi-rs-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function heart5() {
+	heartF = !heartF;
+	if (heartF) {
+		document.getElementById("heart5").classList.remove("fi-rs-heart");
+		document.getElementById("heart5").classList.add("fi-ss-heart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("heart5").classList.remove("fi-ss-heart");
+		document.getElementById("heart5").classList.add("fi-rs-heart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function cart6() {
+	cartF = !cartF;
+	if (cartF) {
+		document.getElementById("cart6").classList.remove("fi-rs-shopping-cart");
+		document.getElementById("cart6").classList.add("fi-ss-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("cart6").classList.remove("fi-ss-shopping-cart");
+		document.getElementById("cart6").classList.add("fi-rs-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function heart6() {
+	heartF = !heartF;
+	if (heartF) {
+		document.getElementById("heart6").classList.remove("fi-rs-heart");
+		document.getElementById("heart6").classList.add("fi-ss-heart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("heart6").classList.remove("fi-ss-heart");
+		document.getElementById("heart6").classList.add("fi-rs-heart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function cart7() {
+	cartF = !cartF;
+	if (cartF) {
+		document.getElementById("cart7").classList.remove("fi-rs-shopping-cart");
+		document.getElementById("cart7").classList.add("fi-ss-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("cart7").classList.remove("fi-ss-shopping-cart");
+		document.getElementById("cart7").classList.add("fi-rs-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function heart7() {
+	heartF = !heartF;
+	if (heartF) {
+		document.getElementById("heart7").classList.remove("fi-rs-heart");
+		document.getElementById("heart7").classList.add("fi-ss-heart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("heart7").classList.remove("fi-ss-heart");
+		document.getElementById("heart7").classList.add("fi-rs-heart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function cart8() {
+	cartF = !cartF;
+	if (cartF) {
+		document.getElementById("cart8").classList.remove("fi-rs-shopping-cart");
+		document.getElementById("cart8").classList.add("fi-ss-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("cart8").classList.remove("fi-ss-shopping-cart");
+		document.getElementById("cart8").classList.add("fi-rs-shopping-cart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
+function heart8() {
+	heartF = !heartF;
+	if (heartF) {
+		document.getElementById("heart8").classList.remove("fi-rs-heart");
+		document.getElementById("heart8").classList.add("fi-ss-heart");
+		document.getElementById("login-ad").classList.add("up");
+	} else {
+		document.getElementById("heart8").classList.remove("fi-ss-heart");
+		document.getElementById("heart8").classList.add("fi-rs-heart");
+		document.getElementById("login-ad").classList.add("up");
+	}
+}
+
 var splide = new Splide(".splide", {
 	type: "loop",
-	autoplay: true,
+	perPage: 1,
+	autoplay: false,
 	classes: {
 		arrows: "splide__arrows",
 		arrow: "splide__arrow",
 		prev: "splide__arrow--prev",
 		next: "splide__arrow--next",
-	},
-	breakpoints: {
-		1350: {
-			perPage: 1,
-		},
-		2440: {
-			perPage: 3,
-		},
 	},
 });
 
